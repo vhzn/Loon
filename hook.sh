600dbb70a39d1729a256283276443260b44cc45c
shylocks <shylocksyang@gmail.com>
2021-01-17 09:54:48 +0800

    测试qx hook

--- hook.sh
+++ hook.sh
@@ -1,3 +1,4 @@
+# Loon
 rm ./loon/shylocks_LoonTask.conf
 echo "hostname = api.m.jd.com, wq.jd.com\n" >> ./loon/shylocks_LoonTask.conf
 
@@ -18,3 +19,21 @@ for file in `ls | grep jd_ $1`
             fi
         done
 git add ./loon/shylocks_LoonTask.conf
+# Quantumultx
+qx=./quantumultx/shylocks_gallery.json
+rm $qx
+echo "{" >> $qx
+echo '  "name": "shylocks task gallery",' >> $qx
+echo '  "description": "https://github.com/LXK9301/jd_scripts",' >> $qx
+echo '  "task": [' >> $qx
+
+for file in `ls | grep jd_ $1`
+        do
+            task=$(cat $file | grep 'tag=.*img-url=.*')
+            echo $task
+            if [ -n "$task" ]; then
+              echo '    "'$task'",'>> $qx
+            fi
+        done
+echo '  ]\n}' >> $qx
+git add $qx
