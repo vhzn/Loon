214ee694093395dc590c29befc474aba9c44ae6f
shylocks <shylocksyang@gmail.com>
2021-01-17 09:36:25 +0800

    测试hook

---
+++ hook.sh
@@ -0,0 +1,16 @@
+rm ./loon/shylocks_LoonTask.conf
+echo "hostname = api.m.jd.com, wq.jd.com\n" >> ./loon/shylocks_LoonTask.conf
+
+for file in `ls | grep jd_ $1`
+        do
+            test=$(cat $file | grep 'cron.*script-path=.*tag=.*')
+            echo $test
+              if [ -n "$test" ]; then
+                var=$(cat $file | grep -oEi 'new Env(.*)')
+                var=${var#*Env\(\'}
+                var='# '${var%\'*}
+                echo $var >> ./loon/shylocks_LoonTask.conf
+                echo $test"\n" >> ./loon/shylocks_LoonTask.conf
+            fi
+        done
+git add ./loon/shylocks_LoonTask.conf
