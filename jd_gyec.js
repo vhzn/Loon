9a5cffec1fb056b19fcfb4d6c7b20c76d34b25c4
shylocks <shylocksyang@gmail.com>
2021-01-19 15:30:00 +0800

    修改提示

--- jd_gyec.js
+++ jd_gyec.js
@@ -277,7 +277,7 @@ function getTaskList() {
                   while ($.strength >= 5) {
                     await beginLevel()
                   }
-                  if($.not3Star.length){
+                  if($.not3Star.length && $.strength >= 5){
                     console.log(`去完成尚未三星的关卡`)
                     for(let level of $.not3Star){
                       $.level = parseInt(level)
