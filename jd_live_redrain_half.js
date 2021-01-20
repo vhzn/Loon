e7d22ce9fddf4824f909d411c905804e8e4fff2f
shylocks <shylocksyang@gmail.com>
2021-01-20 17:48:46 +0800

    自用更新

--- jd_live_redrain_half.js
+++ jd_live_redrain_half.js
@@ -43,7 +43,7 @@ if ($.isNode()) {
   cookiesArr = cookiesArr.filter(item => item !== "" && item !== null && item !== undefined);
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
-
+let ids = {}
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
