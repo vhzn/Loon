321071bafe93b3fbc1f0f8a75ba599f56a42da84
shylocks <shylocksyang@gmail.com>
2021-01-20 13:44:27 +0800

    半点红包雨

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,6 +10,9 @@
 # 直播间红包雨
 0,1 19-21/1 * * * node /scripts/jd_live_redrain2.js >> /scripts/logs/jd_live_redrain2.log 2>&1
 
+# 半点红包雨
+30,31 12-23/1 * * * node /scripts/jd_live_redrain_half.js >> /scripts/logs/jd_live_redrain_half.log 2>&1
+
 # 年货直播红包雨
 0 0,9,11,13,15,17,19,20,21,23 3,5,20-30/1 1,2 * node /scripts/jd_live_redrain_nian.js >> /scripts/logs/jd_live_redrain_nian.log 2>&1
 
