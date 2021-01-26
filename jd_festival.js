56462487fd11ff1806364b7d7002e73832172ea1
shylocks <shylocksyang@gmail.com>
2021-01-26 10:28:18 +0800

    debug

--- jd_festival.js
+++ jd_festival.js
@@ -27,6 +27,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
+const randomCount = $.isNode() ? 20 : 5;
 
 const inviteCodes = [
   `9b98eb88-80ed-40ac-920c-a63fc769e72b@94c2a4d4-b53b-454b-82a0-0b80828bfd37@e274c80b-82dd-470c-878c-0790f5bf6a5d@aae299fc-6854-4fa7-b3ef-a6dedc3771b7@91ae877b-c98b-484a-9143-22d3a70b4088`,
